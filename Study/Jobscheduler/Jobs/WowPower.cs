c72eb117125beec3e6661efb00868d3ca07d8c07
xiongzhenglong <744868887@qq.com>
2017-10-10 07:25:34 +0800

    灌灌灌灌

---
+++ Study/Jobscheduler/Jobs/WowPower.cs
@@ -0,0 +1,98 @@
+﻿using Chloe;
+using Chloe.SQLite;
+using Jobscheduler.Helper;
+using Jobscheduler.Models;
+using QD.Framework;
+using Quartz;
+using SqlliteAccess;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Jobscheduler.Jobs
+{
+    /// <summary>
+    ///
+    /// </summary>
+    [DisallowConcurrentExecution]
+    public class WowPower_RD_Job : IJob
+    {
+        private const string indexRecommendGame = "/indexRecommendGame";
+        private const string indexGame = "/indexGame";
+        private static HttpHelper _helper = new HttpHelper("http://www.wowpower.com");
+
+        private SQLiteContext dbcontext;
+
+        public WowPower_RD_Job()
+        {
+            dbcontext = new SQLiteContext(new SQLiteConnectionFactory("Data Source=" + "dbpath".ValueOfAppSetting() + "\\Chloe.db;Version=3;Pooling=True;Max Pool Size=100;"));
+        }
+        public void Execute(IJobExecutionContext context)
+        {
+            string shortdate = DateTime.Now.ToString("yyyy-MM-dd");
+            DateTime dt = DateTime.Now;
+
+            var t = _helper.Get<WowPower_RD_Api>(null, indexGame);
+            var t2 = _helper.Get<WowPower_RD_Api>(null, indexRecommendGame);
+            List<WowPower_RD> wrlst = new List<WowPower_RD>();
+            IQuery<WowPower_RD> qm = dbcontext.Query<WowPower_RD>();
+            t.data.ForEach(x =>
+            {
+                wrlst.Add(new WowPower_RD()
+                {
+                    clicks = x.clicks,
+                    deposit = x.deposit,
+                    gamename = x.gameName,
+                    hotscore = x.hotScore,
+                    reward = x.reward,
+                    modify = dt,
+                    shortdate = shortdate
+                });
+            });
+            t2.data.ForEach(x =>
+            {
+                wrlst.Add(new WowPower_RD()
+                {
+                    clicks = x.clicks,
+                    deposit = x.deposit,
+                    gamename = x.gameName,
+                    hotscore = x.hotScore,
+                    reward = x.reward,
+                    modify = dt,
+                    shortdate = shortdate
+                });
+            });
+            if (wrlst.Count > 0)
+            {
+                List<WowPower_RD> nowmg = qm.Where(x => x.shortdate == shortdate).ToList();
+                if (nowmg.Count == 0)
+                {
+                    dbcontext.InsertRange(wrlst);
+                }
+                else
+                {
+                    foreach (var item in wrlst)
+                    {
+                        var ddd = nowmg.Where(x => x.gamename == item.gamename).FirstOrDefault();
+                        if (ddd == null)
+                        {
+                            dbcontext.Insert(item);
+                        }
+                        else
+                        {
+                            ddd.clicks = item.clicks;
+                            ddd.deposit = item.deposit;
+                            ddd.hotscore = item.hotscore;
+                            ddd.reward = item.reward;
+                            dbcontext.Update(item);
+                        }
+                    }
+
+                }
+
+            }
+        }
+    }
+}
---
+++ Study/Jobscheduler/Models/WowPower.cs
@@ -0,0 +1,123 @@
+﻿using Chloe.Entity;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Jobscheduler.Models
+{
+    [Table("wowpower_rd")]
+    public class WowPower_RD
+    {
+        [Column(IsPrimaryKey = true)]
+        [AutoIncrement]
+        public int id { get; set; }
+        /// <summary>
+        /// 游戏名称
+        /// </summary>
+        public string gamename { get; set; }
+        /// <summary>
+        /// 游戏热度
+        /// </summary>
+        public double hotscore { get; set; }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        public double deposit { get; set; }
+        /// <summary>
+        /// 周期
+        /// </summary>
+        public double clicks { get; set; }
+        /// <summary>
+        /// 奖金
+        /// </summary>
+        public double reward { get; set; }
+
+        public string shortdate { get; set; }
+
+        /// <summary>
+        /// 修改时间
+        /// </summary>       
+        public DateTime modify { get; set; }
+
+    }
+
+   
+
+
+    public class WowPower_RD_Api
+    {
+        public int resCode { get; set; }
+        public object resMsg { get; set; }
+        public List<WowPower_Data> data { get; set; }
+        public int totalPages { get; set; }
+        public int totalRows { get; set; }
+        public int currentPage { get; set; }
+        public object extra { get; set; }
+    }
+
+    public class WowPower_Data
+    {
+        public string id { get; set; }
+        public long createTime { get; set; }
+        public string createBy { get; set; }
+        public object modifyTime { get; set; }
+        public object modifyBy { get; set; }
+        public object orderBy { get; set; }
+        public string type { get; set; }
+        public string language { get; set; }
+        public string gameDescrip { get; set; }
+        public string gameUrl { get; set; }
+        public int playCounts { get; set; }
+        public int category { get; set; }
+        public object gamePicUrl { get; set; }
+        /// <summary>
+        /// 游戏名
+        /// </summary>
+        public string gameName { get; set; }
+        public int clickNum { get; set; }
+        public int isMember { get; set; }
+        /// <summary>
+        /// 游戏热度
+        /// </summary>
+        public double hotScore { get; set; }
+        public object square { get; set; }
+        public object rectangle { get; set; }
+        /// <summary>
+        /// 
+        /// </summary>
+        public double deposit { get; set; }
+        /// <summary>
+        /// 周期
+        /// </summary>
+        public double clicks { get; set; }
+        /// <summary>
+        /// 奖金
+        /// </summary>
+        public double reward { get; set; }
+        public int punish { get; set; }
+        public int players { get; set; }
+        public string typeName { get; set; }
+        public string platform { get; set; }
+        public string gameSize { get; set; }
+        public string gameUpdateTime { get; set; }
+        public string gameSource { get; set; }
+        public string iosDownloadUrl { get; set; }
+        public string androidDownloadUrl { get; set; }
+        public object tencentDownloadUrl { get; set; }
+        public string gameCoverPic { get; set; }
+        public string gameScreenshotsPic { get; set; }
+        public string gameQrcodePic { get; set; }
+        public string gameCoverPicPath { get; set; }
+        public object gameScreenshotsPicPath { get; set; }
+        public object gameQrcodePicPath { get; set; }
+        public int status { get; set; }
+        public string activityType { get; set; }
+        public long? startTime { get; set; }
+        public long? endTime { get; set; }
+    }
+
+}
